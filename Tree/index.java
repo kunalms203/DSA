dd122646a6360ec48c81f96e960f622103e629ee
kunalms203 <ksreviews8@gmail.com>
2025-04-21 22:58:27 +0530

    level order traversal

--- Tree/index.java
+++ Tree/index.java
@@ -21,12 +21,40 @@ public class index{
             newNode.right=buildTree(nodes);
             return newNode;
         }
+        public static void preorder(Node root){
+            if(root == null){
+                return;
+            }
+            System.out.println(root.data + " ");
+            preorder(root.left);
+            preorder(root.right);
+        }
+        public static void inOrder(Node root){
+            if(root == null){
+                return;
+            }
+            inOrder(root.left);
+            System.out.println(root.data + " ");
+            inOrder(root.right);
+        }
+        public static void postOrder(Node root){
+            if(root == null){
+                return;
+            }
+            postOrder(root.left);
+            postOrder(root.right);
+            System.out.println(root.data + " ");
+        }
     }
+
+    
+
     public static void main(String[] args) {
         int node[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(node);
         System.err.println("");
         System.out.println(root.data);
+        tree.postOrder(root);
     }
 }
